2ff1316cab2d0d6eb9acebceb7518bc2c57dcd92
jchabinec <chabinec.j@gmail.com>
2024-01-09 19:06:15 -0600

    New mapping approach

---
+++ notebooks/scooters.sql
@@ -0,0 +1,46 @@
+SELECT *
+FROM scooters
+WHERE NOT(scooters IS NOT NULL);
+
+SELECT *
+FROM trips
+LIMIT 10;
+
+SELECT 
+    startdate date,
+    COUNT(startdate)
+FROM trips
+GROUP BY startdate;
+
+SELECT COUNT(*)
+FROM scooters;
+
+(SELECT
+    'scooters' subject,
+    MAX(pubdatetime) begin,
+    MIN(pubdatetime) end
+FROM scooters)
+UNION
+(SELECT
+    'trips' subject,
+    MAX(pubtimestamp) begin,
+    MIN(pubtimestamp) end
+FROM trips);
+
+SELECT
+    companyname company,
+    COUNT(DISTINCT sumdid) total_scooters
+FROM scooters
+WHERE sumdid NOT IN
+    (SELECT
+        DISTINCT sumdid
+    FROM trips)
+GROUP BY companyname;
+
+
+SELECT 
+    DISTINCT sumdid,
+    companyname company,
+    ROUND(AVG(COUNT(sumdid)) OVER (PARTITION BY sumdid, DATE(pubtimestamp)),2) avg_daily_usage
+FROM trips
+GROUP BY sumdid, company, pubtimestamp
